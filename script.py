5adb74318efe229279486892c1031e1f7cae58ea
fraantic <fraantic@proton.me>
2026-03-23 20:43:39 +0800

    Added hot fix

--- script.py
+++ script.py
@@ -41,114 +41,116 @@ with open("Ark-Models-main/models_data.json", encoding="utf-8") as file:
 
 
 for name, entry in models_data["data"].items():
-    filenames: dict[str, str] = {k: v[0] if isinstance(v, list) else v for k, v in entry["assetList"].items()}
-    if entry["type"] == "Operator":
-        source_directory = os.path.join("Ark-Models-main", "models", name)
-        file_path = os.path.join(os.getcwd(),source_directory, filenames['.skel'])        
-        with open(file_path, "rb") as f:
-            try:
-                skeleton_data = SkeletonBinary().read_skeleton_data(f.read())
-            except:
+    try:
+        filenames: dict[str, str] = {k: v[0] if isinstance(v, list) else v for k, v in entry["assetList"].items()}
+        if entry["type"] == "Operator":
+            source_directory = os.path.join("Ark-Models-main", "models", name)
+            file_path = os.path.join(os.getcwd(),source_directory, filenames['.skel'])        
+            with open(file_path, "rb") as f:
                 try:
-                    skeleton_data = SkeletonJson().read_skeleton_data(f.read())
+                    skeleton_data = SkeletonBinary().read_skeleton_data(f.read())
                 except:
-                    print("Could'nt parse skeleton data for " + file_path)
-        for a in skeleton_data.animations:
-            output_directory = os.path.join("operator", a.name)
-            os.makedirs(output_directory, exist_ok=True)
-            output_path = os.path.join(output_directory, filenames[".atlas"].replace(".atlas", ".webm"))
-            if os.path.exists(output_path):
-                continue
-                
-            subprocess.run(
-                f"SpineExporter\\SpineExporter.exe "
-                f"--skel {os.path.join(source_directory, filenames['.skel'])} "
-                f"--atlas {os.path.join(source_directory, filenames['.atlas'])} "
-                f'--output {output_path} '
-                f'--animation {a.name} '
-                f"--pma "
-                f"--fps 24 "
-                f"--loop "
-                f"--width 1024 "
-                f"--height 576 "
-                f"--centerx 0 "
-                f"--centery 288",
-                check=True,
-            )
+                    try:
+                        skeleton_data = SkeletonJson().read_skeleton_data(f.read())
+                    except:
+                        print("Could'nt parse skeleton data for " + file_path)
+            for a in skeleton_data.animations:
+                output_directory = os.path.join("operator", a.name)
+                os.makedirs(output_directory, exist_ok=True)
+                output_path = os.path.join(output_directory, filenames[".atlas"].replace(".atlas", ".webm"))
+                if os.path.exists(output_path):
+                    continue
+                    
+                subprocess.run(
+                    f"SpineExporter\\SpineExporter.exe "
+                    f"--skel {os.path.join(source_directory, filenames['.skel'])} "
+                    f"--atlas {os.path.join(source_directory, filenames['.atlas'])} "
+                    f'--output {output_path} '
+                    f'--animation {a.name} '
+                    f"--pma "
+                    f"--fps 24 "
+                    f"--loop "
+                    f"--width 1024 "
+                    f"--height 576 "
+                    f"--centerx 0 "
+                    f"--centery 288",
+                    check=True,
+                )
 
-    elif entry["type"] == "DynIllust":
-        source_directory = os.path.join("Ark-Models-main", "models_illust", name)
-        file_path = os.path.join(os.getcwd(),source_directory, filenames['.skel'])        
-        with open(file_path, "rb") as f:
-            try:
-                skeleton_data = SkeletonBinary().read_skeleton_data(f.read())
-            except:
+        elif entry["type"] == "DynIllust":
+            source_directory = os.path.join("Ark-Models-main", "models_illust", name)
+            file_path = os.path.join(os.getcwd(),source_directory, filenames['.skel'])        
+            with open(file_path, "rb") as f:
                 try:
-                    skeleton_data = SkeletonJson().read_skeleton_data(f.read())
+                    skeleton_data = SkeletonBinary().read_skeleton_data(f.read())
                 except:
-                    print("Could'nt parse skeleton data for " + file_path)
-        for a in skeleton_data.animations:
-            output_directory = os.path.join("illust", a.name)
-            os.makedirs(output_directory, exist_ok=True)
-            output_path = os.path.join(output_directory, filenames[".atlas"].replace(".atlas", ".webm"))
-            if os.path.exists(output_path):
-                continue
+                    try:
+                        skeleton_data = SkeletonJson().read_skeleton_data(f.read())
+                    except:
+                        print("Could'nt parse skeleton data for " + file_path)
+            for a in skeleton_data.animations:
+                output_directory = os.path.join("illust", a.name)
+                os.makedirs(output_directory, exist_ok=True)
+                output_path = os.path.join(output_directory, filenames[".atlas"].replace(".atlas", ".webm"))
+                if os.path.exists(output_path):
+                    continue
 
-            print(output_path)
-            subprocess.run(
-                f"SpineExporter/SpineExporter.exe "
-                f"--skel {os.path.join(source_directory, filenames['.skel'])} "
-                f"--atlas {os.path.join(source_directory, filenames['.atlas'])} "
-                f'--output {output_path} '
-                f'--animation {a.name} '
-                f"--pma "
-                f"--fps 24 "
-                f"--crf 40 "
-                f"--loop "
-                f"--width 1024 "
-                f"--height 1024 "
-                f"--centerx 0 "
-                f"--centery 1024 "
-                f"--zoom 0.5",
-                check=True,
-            )
-        
-        
-    elif entry["type"] == "Enemy":
-        source_directory = os.path.join("Ark-Models-main", "models_enemies", name)
-        file_path = os.path.join(os.getcwd(),source_directory, filenames['.skel'])        
-        with open(file_path, "rb") as f:
-            try:
-                skeleton_data = SkeletonBinary().read_skeleton_data(f.read())
-            except:
+                print(output_path)
+                subprocess.run(
+                    f"SpineExporter/SpineExporter.exe "
+                    f"--skel {os.path.join(source_directory, filenames['.skel'])} "
+                    f"--atlas {os.path.join(source_directory, filenames['.atlas'])} "
+                    f'--output {output_path} '
+                    f'--animation {a.name} '
+                    f"--pma "
+                    f"--fps 24 "
+                    f"--crf 40 "
+                    f"--loop "
+                    f"--width 1024 "
+                    f"--height 1024 "
+                    f"--centerx 0 "
+                    f"--centery 1024 "
+                    f"--zoom 0.5",
+                    check=True,
+                )
+            
+            
+        elif entry["type"] == "Enemy":
+            source_directory = os.path.join("Ark-Models-main", "models_enemies", name)
+            file_path = os.path.join(os.getcwd(),source_directory, filenames['.skel'])        
+            with open(file_path, "rb") as f:
                 try:
-                    skeleton_data = SkeletonJson().read_skeleton_data(f.read())
+                    skeleton_data = SkeletonBinary().read_skeleton_data(f.read())
                 except:
-                    print("Could'nt parse skeleton data for " + file_path)
-        for a in skeleton_data.animations:
-            output_directory = os.path.join("enemies", a.name)
-            os.makedirs(output_directory, exist_ok=True)
-            output_path = os.path.join(output_directory, filenames[".atlas"].replace(".atlas", ".webm"))
-            if os.path.exists(output_path):
-                continue
+                    try:
+                        skeleton_data = SkeletonJson().read_skeleton_data(f.read())
+                    except:
+                        print("Could'nt parse skeleton data for " + file_path)
+            for a in skeleton_data.animations:
+                output_directory = os.path.join("enemies", a.name)
+                os.makedirs(output_directory, exist_ok=True)
+                output_path = os.path.join(output_directory, filenames[".atlas"].replace(".atlas", ".webm"))
+                if os.path.exists(output_path):
+                    continue
 
-            print(output_path)
-            subprocess.run(
-                f"SpineExporter\\SpineExporter.exe "
-                f"--skel {os.path.join(source_directory, filenames['.skel'])} "
-                f"--atlas {os.path.join(source_directory, filenames['.atlas'])} "
-                f'--output {output_path} '
-                f'--animation {a.name} '
-                f"--pma "
-                f"--fps 24 "
-                f"--loop "
-                f"--width 1024 "
-                f"--height 576 "
-                f"--centerx 0 "
-                f"--centery 288",
-                check=True,
-            )
-
+                print(output_path)
+                subprocess.run(
+                    f"SpineExporter\\SpineExporter.exe "
+                    f"--skel {os.path.join(source_directory, filenames['.skel'])} "
+                    f"--atlas {os.path.join(source_directory, filenames['.atlas'])} "
+                    f'--output {output_path} '
+                    f'--animation {a.name} '
+                    f"--pma "
+                    f"--fps 24 "
+                    f"--loop "
+                    f"--width 1024 "
+                    f"--height 576 "
+                    f"--centerx 0 "
+                    f"--centery 288",
+                    check=True,
+                )
+    except:
+        print("Something broke with " + name + " " + entry)
 
 if (os.path.exists(os.path.join(os.getcwd(),"Ark-Models-main"))):
     shutil.rmtree(os.path.join(os.getcwd(),"Ark-Models-main"))
